593561641e144305aa002f32ee5c2e862ccc761f
m0dulo <cuckooegret@gmail.com>
2019-08-22 22:19:44 +0800

    update driver.h

--- src/model/driver.h
+++ src/model/driver.h
@@ -9,7 +9,7 @@ class Driver {
 public:
     ModelParams model_params_;
     HyperParams hyper_params_;
-    Metric eval_;
+    // Metric eval_;
     ModelUpdate ada_;
 
     void init() {
@@ -25,8 +25,10 @@ public:
         setUpdateParameters(hyper_params_.nn_regular, hyper_params_.ada_alpha, hyper_params_.ada_eps);
     }
 
-    dtype train(Graph &graph, const std::vector<Example> &examples, int iter) {
+    std::tuple<dtype, double, dtype> train(Graph &graph, const std::vector<Example> &examples, int iter) {
         dtype cost = 0.0;
+        int correct_num = 0;
+        int total_num = 0;
         std::vector<std::vector<Node *>> batch_word_nodes;
         for (auto example : examples){
             std::vector<Node *> word_nodes;
@@ -38,21 +40,29 @@ public:
         graph.compute();
         for (int i = 0; i < examples.size(); i++) {
             std::pair<dtype, std::vector<int>> loss = MaxLogProbabilityLoss(batch_word_nodes.at(i), examples.at(i).m_words_id_, examples.size());
+            correct_num += getCorrectNum(examples.at(i).m_words_id_, loss.second);
+            total_num += examples.at(i).m_words_id_.size();
             cost += loss.first;
         }
         graph.backward();
+        double accuracy = correct_num * 1.0 / total_num;
         dtype perplexity = exp(cost);
-        return perplexity;
+        
+        return std::make_tuple(cost, accuracy, perplexity);
     }
 
-    dtype predict(Graph &graph, const Feature &feature) {
+    std::tuple<dtype, double, dtype>predict(Graph &graph, const Feature &feature, std::vector<int> &answer) { 
         std::vector<Node *> word_nodes;
         GraphBuilder builder;
-        builder.forward(graph, model_params_, hyper_params_, feature, true, word_nodes);
+        builder.forward(graph, model_params_, hyper_params_, feature, false, word_nodes);
         graph.compute();
-        std::pair<dtype, std::vector<int>> loss = MaxLogProbabilityLoss(word_nodes, instance.m_words_id_, 1);
+        std::pair<dtype, std::vector<int>> loss = MaxLogProbabilityLoss(word_nodes, answer, 1);
+        int correct_num = getCorrectNum(answer, loss.second);
+        int total_num = word_nodes.size();
+        dtype cost = loss.first;
+        double accuracy = correct_num * 1.0 / total_num;
         dtype perplexity = exp(loss.first);
-        return perplexity;
+        return std::make_tuple(cost, accuracy, perplexity);
     }
 
     void updateModel() {
@@ -66,8 +76,17 @@ public:
     }
 
 private:
-    void resetEval() {
-        eval_.reset();
+    // void resetEval() {
+    //     eval_.reset();
+    // }
+
+    int getCorrectNum(std::vector<int> &answer, std::vector<int> &predict) {
+        int correct_num = 0;
+        for (int i = 0; i < predict.size(); i++) {
+            if (predict.at(i) == answer.at(i))
+                correct_num += 1;
+        }
+        return correct_num;
     }
 
     void setUpdateParameters(dtype nnRegular, dtype adaAlpha, dtype adaEps) {
